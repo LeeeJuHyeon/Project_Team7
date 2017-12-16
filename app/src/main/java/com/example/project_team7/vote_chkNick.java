ab70eabb6fae6893fd0a33cb781f47c633ae5d65
LeeeJuHyeon <leesop0602@naver.com>
2017-12-16 19:50:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/project_team7/vote_chkNick.java
@@ -0,0 +1,10 @@
+package com.example.project_team7;
+
+import com.android.volley.toolbox.StringRequest;
+
+/**
+ * Created by 주현 on 2017-12-16.
+ */
+
+public class vote_chkNick extends StringRequest {
+}
